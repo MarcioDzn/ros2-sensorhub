c074491c7752e16034719bdb5164ca360377055a
MarcioDzn <marcio.roberto0739@gmail.com>
2025-11-17 21:08:16 -0300

    WIP: add client node

--- src/clienthub/src/client_node.cpp
+++ src/clienthub/src/client_node.cpp
@@ -1,10 +1,46 @@
 #include <cstdio>
 
+#include "rclcpp/rclcpp.hpp"
+#include "interfaces/srv/set_motor_config.hpp"
+
+using SetMotorConfig = interfaces::srv::SetMotorConfig;
+
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
+  rclcpp::init(argc, argv);
 
-  printf("hello world clienthub package\n");
+  if (argc != 3) {
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: send config to motor node");
+      return 1;
+  }
+
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("manipulate_motor_client");
+
+  rclcpp::Client<SetMotorConfig>::SharedPtr client =
+    node->create_client<SetMotorConfig>("manipulate_motor");
+
+  auto request = std::make_shared<SetMotorConfig::Request>();
+  request->a = atoll(argv[1]);
+  request->b = atoll(argv[2]);
+
+  while (!client->wait_for_service(1s)) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+      return 0;
+    }
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+  }
+
+  auto result = client->async_send_request(request);
+  // Wait for the result.
+  if (rclcpp::spin_until_future_complete(node, result) ==
+    rclcpp::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->sum);
+  } else {
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
+  }
+
+  rclcpp::shutdown();
   return 0;
 }
