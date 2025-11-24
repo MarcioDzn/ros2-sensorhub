f86cfc4ae3d5c0ad5d291fb8f055a54e08dba883
MarcioDzn <marcio.roberto0739@gmail.com>
2025-11-24 11:40:51 -0300

    feat: add client node

--- src/clienthub/src/client_node.cpp
+++ src/clienthub/src/client_node.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <chrono>
 
 #include "rclcpp/rclcpp.hpp"
 #include "interfaces/srv/set_motor_config.hpp"
@@ -9,11 +10,6 @@ int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
 
-  if (argc != 3) {
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: config motor");
-      return 1;
-  }
-
   std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("motor_config_client");
 
   rclcpp::Client<SetMotorConfig>::SharedPtr client =
@@ -21,7 +17,7 @@ int main(int argc, char ** argv)
 
   auto request = std::make_shared<SetMotorConfig::Request>();
 
-  while (!client->wait_for_service(1s)) {
+  while (!client->wait_for_service(std::chrono::seconds(1))) {
     if (!rclcpp::ok()) {
       RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
       return 0;
@@ -29,6 +25,18 @@ int main(int argc, char ** argv)
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
   }
 
+
+  std::string command = argv[1];
+  request->command = command;
+  
+    uint16_t motor_id = static_cast<uint16_t>(std::stoul(argv[2]));  
+    request->motor_id = motor_id;
+  
+  for (int i = 3; i < argc; i++)
+  {
+    request->params.push_back(std::stoll(argv[i]));
+  }
+  
   auto result = client->async_send_request(request);
   // Wait for the result.
   if (rclcpp::spin_until_future_complete(node, result) ==
