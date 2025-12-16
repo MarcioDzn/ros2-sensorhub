8263b11e742fbcabcc004cff60ce4ebfba4ba460
MarcioDzn <marcio.roberto0739@gmail.com>
2025-12-16 11:01:20 -0300

    feat: add launch parameters .yaml file

--- src/sensorhub_bringup/launch/base.launch.py
+++ src/sensorhub_bringup/launch/base.launch.py
@@ -1,12 +1,19 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
 
 def generate_launch_description():
+    params_file = PathJoinSubstitution([
+        FindPackageShare('sensorhub_bringup'),
+        'config',
+        'imu.yaml'
+    ])
     return LaunchDescription([
         Node(
             package='imu_driver',
             executable='imu_node',
             output='screen',
-            arguments=['--ros-args', '--log-level', 'info']
+            parameters=[params_file]
         )
     ])
