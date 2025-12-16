e96c2bf3ec26faacf80ee7d9cfb7ab2a583295e1
MarcioDzn <marcio.roberto0739@gmail.com>
2025-12-16 11:08:50 -0300

    feat: add pressure node to launch file

--- src/sensorhub_bringup/launch/base.launch.py
+++ src/sensorhub_bringup/launch/base.launch.py
@@ -4,16 +4,29 @@ from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution
 
 def generate_launch_description():
-    params_file = PathJoinSubstitution([
+    imu_params = PathJoinSubstitution([
         FindPackageShare('sensorhub_bringup'),
         'config',
         'imu.yaml'
     ])
+
+    pressure_params = PathJoinSubstitution([
+        FindPackageShare('sensorhub_bringup'),
+        'config',
+        'pressure.yaml'
+    ])
+
     return LaunchDescription([
         Node(
             package='imu_driver',
             executable='imu_node',
             output='screen',
-            parameters=[params_file]
+            parameters=[imu_params]
+        ),
+
+        Node(
+            package='pressure_driver',
+            executable='pressure_node',
+            output='screen',
         )
     ])
