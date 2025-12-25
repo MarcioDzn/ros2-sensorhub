103c538ada8ed5013d5ae377fd5b529cec80e733
MarcioDzn <marcio.roberto0739@gmail.com>
2025-12-25 12:24:26 -0300

    fix: dynamixel link constructor

--- src/actuator_comm/include/actuator_comm/link/dynamixel_link.hpp
+++ src/actuator_comm/include/actuator_comm/link/dynamixel_link.hpp
@@ -14,10 +14,11 @@
 class DynamixelLink : public ActuatorLink
 {
     public:
-        DynamixelLink()
-            : ActuatorLink(
-                std::make_shared<DynamixelProtocol>(),
-                std::make_shared<SerialHandler>()) {}
+        DynamixelLink(std::shared_ptr<SerialHandler> transport, 
+                      std::shared_ptr<ActuatorProtocol> protocol)
+            : ActuatorLink(protocol, transport) {} 
+
+        DynamixelLink() = default;
 
         int write1Byte(uint8_t id, uint8_t address, uint8_t data) override;
         int write2Byte(uint8_t id, uint8_t address, uint16_t data) override;
