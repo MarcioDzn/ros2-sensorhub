eda8eaede1d53f23600b7258ab8368636c38e4d3
MarcioDzn <marcio.roberto0739@gmail.com>
2025-12-26 12:12:26 -0300

    fix: readPacket function checksum

--- src/actuator_comm/src/link/dynamixel_link.cpp
+++ src/actuator_comm/src/link/dynamixel_link.cpp
@@ -98,86 +98,80 @@ int DynamixelLink::sendPacketAndReadStatus(
 
 int DynamixelLink::readPacket(std::array<uint8_t, RXPACKET_MAX_LEN>& packet)
 {
-	int result = -1;
-
-	size_t packet_size = 6; // tamanho minimo (HEADER0 HEADER1 ID LENGTH ERROR CHKSUM)
 	size_t read_size = 0;
-	uint8_t checksum = 0;
+	size_t wait_length = 6; // tamanho minimo (HEADER0 HEADER1 ID LENGTH ERROR CHKSUM)
 
 	auto start = std::chrono::steady_clock::now();
-  	constexpr auto TIMEOUT = std::chrono::milliseconds(20);
+	constexpr auto TIMEOUT = std::chrono::milliseconds(20);
 
-	while(true)
+	while (true)
 	{
 		if (std::chrono::steady_clock::now() - start > TIMEOUT)
-      		return -2; // timeout
+			return -2;
 
 		ssize_t n = transport_->readData(packet.data() + read_size,
-											packet_size - read_size);
+			wait_length - read_size);
 
-		if (n <= 0)
+		if (n > 0)
+			read_size += static_cast<size_t>(n);
+
+		// se ainda não foi lido 6 bytes, volta a ler
+		if (read_size < wait_length)
 			continue;
 
-		read_size += static_cast<size_t>(n);
-
-		// se já tiver pego todos os dados importantes
-		// ou pelo menos a quantidade necessária
-		if (read_size >= packet_size)
+		// encontra a posição do header no pacote
+		uint8_t idx = 0;
+		for (idx = 0; idx < (read_size - 1); idx++)
 		{
-			uint8_t i = 0;
-			for (i = 0; i < (read_size-1); i++)
-				if (packet[i] == 0xFF && packet[i+1] == 0xFF) 
-					break;
-
-			if (i == 0)
-			{
-				if (packet[ID_POS] > 0xFD ||                  // unavailable ID
-					packet[LENGTH_POS] > RXPACKET_MAX_LEN ||  // unavailable Length
-					packet[ERROR_POS] > 0x7F)                 // unavailable Error
-				{
-					// remove o primeiro byte no pacote
-					for (uint16_t s = 0; s < read_size - 1; s++)
-						packet[s] = packet[1 + s];
-					read_size -= 1;
-					continue;
-				}
-
-				// recalcula o tamanho exato do pacote
-				if (packet_size != static_cast<size_t>(packet[LENGTH_POS] + LENGTH_POS + 1))
-				{
-					packet_size = packet[LENGTH_POS] + LENGTH_POS + 1;
-					continue;
-				}
-
-				if (read_size < packet_size)
-				{
-					// TODO: checar timeout
-					continue;
-				}
-
-				// calcula checksum
-				checksum = 0;
-				for (uint16_t i = 2; i < packet_size - 1; i++)   // exceto header, checksum
-					checksum += packet[i];
-				checksum = ~checksum;
-
-				// verifica checksum
-				if (packet[packet_size - 1] == checksum)
-				{result = 0;}
-				else
-				{result = -1;}
+			if (packet[idx] == 0xFF && packet[idx + 1] == 0xFF)
 				break;
-				
-			} else 
+		}
+		
+		// se o header estiver no inicio
+		if (idx == 0)
+		{
+			if (packet[2] > 0xFD ||
+				packet[3] > RXPACKET_MAX_LEN ||
+				packet[4] > 0x7F)
 			{
-				for (uint8_t s = 0; s < read_size - i; s++)
-				{packet[s] = packet[s+i];}
-				read_size -= i;
+				std::memmove(packet.data(), packet.data() + 1, read_size - 1);
+				read_size -= 1;
+				wait_length = 6; // reseta para busca de header
+				continue;
 			}
+
+			// recalcula o tamanho real do pacote
+			// para comportar os parâmetros
+			size_t total_expected = packet[3] + 4;
+
+			if (wait_length != total_expected)
+			{
+				wait_length = total_expected;
+				// volta para o loop pra ler o resto dos parâmetros
+				continue; 
+			}
+
+			// verifica o checksum
+			uint8_t checksum = 0;
+			for (size_t i = 2; i < wait_length - 1; i++)
+				checksum += packet[i];
+			checksum = ~checksum;
+
+			// verifica se o checksum bate
+			std::cout << "DEBUG - ID: " << (int)packet[2] << " LEN: " << (int)packet[3] << std::endl;
+			std::cout << "DEBUG - Calculado: " << std::hex << (int)checksum 
+          	<< " Recebido: " << (int)packet[wait_length - 1] << std::dec << std::endl;
+			if (packet[wait_length - 1] == checksum)
+				return 0;
+			else	
+				return -1;
+		} else 
+		{
+			// move os dados, fazendo o header ficar no inicio
+			std::memmove(packet.data(), packet.data() + idx, read_size - idx);
+			read_size -= idx;
 		}
 	}
-
-	return result;
 }
 
 int DynamixelLink::readStatus(uint8_t id, StatusPacket& out)
