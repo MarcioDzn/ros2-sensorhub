eb7702a68257d976577baf89c92e6324a2404ab9
MarcioDzn <marcio.roberto0739@gmail.com>
2025-12-27 11:31:51 -0300

    feat: add dynamixel controller

---
+++ src/actuator_comm/include/actuator_comm/controller/actuator_controller.hpp
@@ -0,0 +1,15 @@
+#ifndef ACTUATOR_CONTROLLER_HPP
+#define ACTUATOR_CONTROLLER_HPP
+
+#include <cstdint>
+
+class ActuatorController
+{
+    public:
+        virtual int init(std::string device, int baudrate) = 0;
+        virtual int setTorque(uint8_t id, uint8_t enable_torque) = 0;
+        virtual int setGoalPosition(uint8_t id, uint16_t goal_pos) = 0;
+        virtual int getCurrentPosition(uint8_t id, uint16_t& curr_pos) = 0;
+};
+
+#endif // ACTUATOR_CONTROLLER_HPP
---
+++ src/actuator_comm/include/actuator_comm/controller/dynamixel_controller.hpp
@@ -0,0 +1,27 @@
+#ifndef DYNAMIXEL_CONTROLLER_HPP
+#define DYNAMIXEL_CONTROLLER_HPP
+
+#include <string>
+#include <memory>
+
+#include "actuator_comm/controller/actuator_controller.hpp"
+#include "actuator_comm/link/actuator_link.hpp"
+#include "actuator_comm/link/dynamixel_link.hpp"
+#include "actuator_comm/protocol/actuator_protocol.hpp"
+#include "actuator_comm/protocol/dynamixel_protocol.hpp"
+
+class DynamixelController : public ActuatorController
+{
+    public:
+        explicit DynamixelController();
+
+        int init(std::string device, int baudrate) override;
+        int setTorque(uint8_t id, uint8_t enable_torque) override;
+        int setGoalPosition(uint8_t id, uint16_t goal_pos) override;
+        int getCurrentPosition(uint8_t id, uint16_t& curr_pos) override;
+
+    private:
+        std::shared_ptr<DynamixelLink> link_;
+};
+
+#endif // DYNAMIXEL_CONTROLLER_HPP
