0274574f690ba60b9e861a05694e5b1c318ad352
MarcioDzn <marcio.roberto0739@gmail.com>
2026-01-13 15:02:42 -0300

    feat: add torque to Actuator model

--- src/nodes/actuator_node/include/actuator_node/model/actuator.hpp
+++ src/nodes/actuator_node/include/actuator_node/model/actuator.hpp
@@ -11,15 +11,18 @@ class Actuator
         explicit Actuator(uint8_t id);
         virtual ~Actuator();
 
-        void set_position(uint16_t position);
+        int set_position(uint16_t position);
+        void set_torque_status(bool status);
 
         const uint8_t get_id() const;
         const uint16_t get_position() const;
         const uint16_t get_position_deg() const;
+        const bool get_torque_status() const;
 
     private:
         uint8_t id_;
         uint16_t position_;
+        bool torque_;
 
 };
 
--- src/nodes/actuator_node/src/model/actuator.cpp
+++ src/nodes/actuator_node/src/model/actuator.cpp
@@ -3,14 +3,21 @@
 Actuator::Actuator(uint8_t id) {
     id_ = id;
     position_ = 0;
+    torque_ = false;
 }
 
-void Actuator::set_position(uint16_t position)
+int Actuator::set_position(uint16_t position)
 {
     if (position < 0 || position > MAX_POSITION)
-        return;
+        return -1;
 
     position_ = position;
+    return 0;
+}
+
+void Actuator::set_torque_status(bool status)
+{
+    torque_ = status;
 }
 
 const uint8_t Actuator::get_id() const
@@ -26,6 +33,11 @@ const uint16_t Actuator::get_position() const
 const uint16_t Actuator::get_position_deg() const
 {
     return position_ * (360 / MAX_POSITION);
+}
+
+const bool Actuator::get_torque_status() const
+{
+    return torque_;
 }
 
 Actuator::~Actuator() {}
