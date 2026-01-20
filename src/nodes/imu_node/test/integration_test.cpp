e90b9bf1d6277da9a784d585fc21d3c5fdbc12e6
MarcioDzn <marcio.roberto0739@gmail.com>
2026-01-20 11:49:02 -0300

    test: add mux conflict read euler success

--- src/nodes/imu_node/test/integration_test.cpp
+++ src/nodes/imu_node/test/integration_test.cpp
@@ -62,15 +62,16 @@ class IMUFixture : public ::testing::Test {
 
         mux_registers[0][0][40][IMU::BNO055_CHIP_ID_ADDR] = BNO055_ID; 
         mux_registers[0][0][41][IMU::BNO055_CHIP_ID_ADDR] = BNO055_ID;
+        mux_registers[0][1][40][IMU::BNO055_CHIP_ID_ADDR] = BNO055_ID;
 
         sub_node = std::make_shared<rclcpp::Node>("test_subscriber");
 
         rclcpp::NodeOptions options;
         options.append_parameter_override("base_name", "imu");
         options.append_parameter_override("update_rate_ms", 15);
-        options.append_parameter_override("ids", std::vector<int64_t>{1, 2});
-        options.append_parameter_override("multiplexer", std::vector<int64_t>{0, 1});
-        options.append_parameter_override("addresses", std::vector<int64_t>{40, 41});
+        options.append_parameter_override("ids", std::vector<int64_t>{1, 2, 3});
+        options.append_parameter_override("multiplexer", std::vector<int64_t>{0, 1, 0});
+        options.append_parameter_override("addresses", std::vector<int64_t>{40, 41, 40});
 
         node = std::make_shared<IMUNode>(options);
     }
@@ -161,4 +162,43 @@ TEST_F(IMUFixture, read_multisensor_euler_success)
     // 320 / 16.0 = 20.0
     EXPECT_NEAR(received_msg->imus[0].roll, 20.0f, 0.05);
     EXPECT_NEAR(received_msg->imus[1].roll, 20.0f, 0.05);
+}
+
+TEST_F(IMUFixture, read_conflict_mux_euler_success)
+{
+    // simula dados de Euler no mapa (Roll = 20 graus)
+    // 20.0 * 16 = 320 -> 0x0140
+    // sensor de id = 1 (roll)
+    mux_registers[0][0][40][IMU::BNO055_EULER_R_LSB_ADDR] = 0x40;
+    mux_registers[0][0][40][IMU::BNO055_EULER_R_MSB_ADDR] = 0x01; 
+
+    // sensor de id = 3 (roll) (mesmo endereço do sensor 1)
+    mux_registers[0][1][40][IMU::BNO055_EULER_R_LSB_ADDR] = 0x40;
+    mux_registers[0][1][40][IMU::BNO055_EULER_R_MSB_ADDR] = 0x01; 
+
+
+    using IMUState = interfaces::msg::IMUState;
+    IMUState::SharedPtr received_msg = nullptr;
+
+    auto qos = rclcpp::QoS(rclcpp::KeepLast(10))
+        .best_effort()
+        .durability_volatile();
+    auto sub = sub_node->create_subscription<IMUState>(
+        "imu/state", qos, [&](IMUState::SharedPtr msg) {
+            received_msg = msg;
+        });
+
+    auto start_time = std::chrono::steady_clock::now();
+    while (!received_msg && (std::chrono::steady_clock::now() - start_time < 2s)) {
+        rclcpp::spin_some(node);
+        rclcpp::spin_some(sub_node);
+        std::this_thread::sleep_for(10ms);
+    }
+
+    ASSERT_NE(received_msg, nullptr) << "Timeout: O nó não publicou nada!";
+    ASSERT_FALSE(received_msg->imus.empty());
+    
+    // 320 / 16.0 = 20.0
+    EXPECT_NEAR(received_msg->imus[0].roll, 20.0f, 0.05);
+    EXPECT_NEAR(received_msg->imus[2].roll, 20.0f, 0.05);
 }
