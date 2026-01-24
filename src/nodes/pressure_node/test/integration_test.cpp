5f1470be95b012f7aee3a8f0ca3195057eda0d42
MarcioDzn <marcio.roberto0739@gmail.com>
2026-01-24 11:03:12 -0300

    test: add pressure tests

---
+++ src/nodes/pressure_node/test/integration_test.cpp
@@ -0,0 +1,177 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <rclcpp/rclcpp.hpp>
+
+#include <pty.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "pressure_node.hpp"
+
+#include "interfaces/msg/pressure_state.hpp"
+
+using namespace std::chrono_literals;
+
+class PressureNodeFixture : public ::testing::Test {
+    protected:
+        int master_fd;
+        int slave_fd;
+        char slave_name[100];
+        std::shared_ptr<PressureNode> node;
+
+    void SetUp() override {
+        if (!rclcpp::ok()) {
+            rclcpp::init(0, nullptr);
+        }
+
+        ASSERT_EQ(0, openpty(&master_fd, &slave_fd, slave_name, nullptr, nullptr));
+
+        close(slave_fd);
+
+        rclcpp::NodeOptions options;
+        options.append_parameter_override("base_name", "pressure");
+        options.append_parameter_override("update_rate_ms", 15);
+        options.append_parameter_override("usb_ports", std::vector<std::string>{slave_name});
+        options.append_parameter_override("baudrate", 115200);
+        options.append_parameter_override("ids", std::vector<int64_t>{1});
+
+        node = std::make_shared<PressureNode>(options);
+    }
+
+    void TearDown() {
+        node.reset();
+        close(master_fd);
+        close(slave_fd);
+    }
+
+    bool waitFor(std::function<bool()> condition, std::chrono::milliseconds timeout = 100ms) {
+        auto start = std::chrono::steady_clock::now();
+        while ((std::chrono::steady_clock::now() - start) < timeout) {
+            if (condition()) return true;
+            rclcpp::spin_some(node);
+            std::this_thread::sleep_for(10ms);
+        }
+        return false;
+    }
+};
+
+TEST_F(PressureNodeFixture, publishes_data_success) {
+    bool received = false;
+
+    auto qos = rclcpp::QoS(rclcpp::KeepLast(10))
+        .best_effort()
+        .durability_volatile();
+
+    auto sub = node->create_subscription<interfaces::msg::PressureState>(
+        "pressure/state",
+        qos,
+        [&received](const interfaces::msg::PressureState::SharedPtr) {
+            received = true;
+        }
+    );
+
+    // Executor para processar callbacks
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node);
+
+    // Mock do pacote vindo da serial
+    std::string dummy_response =
+        "1234 1234 1234 1234 1234 1234 1234 1234 "
+        "1234 1234 1234 1234 1234 1234 1234 1234";
+
+    dummy_response.push_back('\0');
+
+    write(master_fd, dummy_response.c_str(), dummy_response.size());
+
+    auto timeout = std::chrono::milliseconds(500);
+    auto start = std::chrono::steady_clock::now();
+
+    while (!received && (std::chrono::steady_clock::now() - start < timeout)) {
+        executor.spin_some();
+        std::this_thread::sleep_for(10ms);
+    }
+
+    ASSERT_TRUE(received);
+}
+
+TEST_F(PressureNodeFixture, publishes_data_content_success) {
+    bool received = false;
+    int8_t received_id = 0;
+    interfaces::msg::PressureData received_pressure_data;
+
+    auto qos = rclcpp::QoS(rclcpp::KeepLast(10))
+        .best_effort()
+        .durability_volatile();
+
+    auto sub = node->create_subscription<interfaces::msg::PressureState>(
+        "pressure/state",
+        qos,
+        [&received, &received_id, &received_pressure_data](const interfaces::msg::PressureState::SharedPtr msg) {
+            received = true;
+            received_id = msg->ids[0];
+            received_pressure_data = msg->pressures[0];
+        }
+    );
+
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node);
+
+    std::string dummy_response =
+        "123  1234 1234 1234 1234 1234 1234 1234 "
+        "1234 1234 1234 1234 1234 1234 1234 1234";
+    dummy_response.push_back('\0');
+
+    write(master_fd, dummy_response.c_str(), dummy_response.size());
+
+    auto timeout = std::chrono::milliseconds(500);
+    auto start = std::chrono::steady_clock::now();
+
+    while (!received && (std::chrono::steady_clock::now() - start < timeout)) {
+        executor.spin_some();
+        std::this_thread::sleep_for(10ms);
+    }
+
+    ASSERT_TRUE(received);
+    EXPECT_EQ(received_id, 1);
+
+    ASSERT_FALSE(received_pressure_data.pressures.empty());
+    EXPECT_EQ(received_pressure_data.pressures[0], 123);
+}
+
+/*
+TEST_F(PressureNodeFixture, publishes_junk_data) {
+    bool received = false;
+
+    auto qos = rclcpp::QoS(rclcpp::KeepLast(10))
+        .best_effort()
+        .durability_volatile();
+
+    auto sub = node->create_subscription<interfaces::msg::PressureState>(
+        "pressure/state",
+        qos,
+        [&received](const interfaces::msg::PressureState::SharedPtr msg) {
+            received = true;
+        }
+    );
+
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node);
+
+    std::string dummy_response =
+        "abcd abr  ###  123  1234 1234 1234 1234 1234 1234 1234 "
+        "1234 1234 1234 1234 1234 1234 1234 1234";
+    dummy_response.push_back('\0');
+
+    write(master_fd, dummy_response.c_str(), dummy_response.size());
+
+    auto timeout = std::chrono::milliseconds(500);
+    auto start = std::chrono::steady_clock::now();
+
+    while (!received && (std::chrono::steady_clock::now() - start < timeout)) {
+        executor.spin_some();
+        std::this_thread::sleep_for(10ms);
+    }
+
+    ASSERT_FALSE(received) << "No publicou dados que deveriam estar corrompidos";
+}
+*/
