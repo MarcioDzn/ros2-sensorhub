0cb6137676bb172df0f859d31e65d42fce5fc09d
MarcioDzn <marcio.roberto0739@gmail.com>
2026-01-24 13:27:34 -0300

    feat: implement pressure driver

---
+++ src/nodes/pressure_node/src/driver/pressure_driver.cpp
@@ -0,0 +1,65 @@
+#include "driver/pressure_driver.hpp"
+
+#include <sstream>
+
+PressureDriver::PressureDriver() {}
+
+int PressureDriver::init(std::string device, int baudrate)
+{
+    transport_ = std::make_unique<SerialHandler>();
+    if (transport_->init(device.c_str()) < 0)
+        return -1; 
+    transport_->setDefaultConfig();
+    transport_->setBaudRate(baudrate);
+
+    return 0;
+}
+
+int PressureDriver::getData(std::vector<uint16_t>& data)
+{
+    char buffer[BUFFER_SIZE];
+
+    if (readCString(buffer, BUFFER_SIZE)) {
+        return -1; 
+    }
+
+    data = parseNumbersFromString(std::string(buffer));
+
+    if (data.empty()) {
+        return -2;
+    }
+    return 0; 
+}
+
+std::vector<uint16_t> PressureDriver::parseNumbersFromString(
+    const std::string& input)
+{
+    std::vector<uint16_t> values;
+    std::stringstream ss(input);
+    std::string token;
+    
+    while (ss >> token) {
+        try {
+            values.push_back(static_cast<uint16_t>(std::stoul(token)));
+        } catch (...) {
+            continue;
+        }
+    } 
+    return values;
+}
+
+bool PressureDriver::readCString(char* buffer, size_t max_size)
+{
+    size_t i = 0;
+    char c;
+    
+    while(i < max_size-1)
+    {
+        ssize_t n = transport_->readData(&c, 1);
+        if (n <= 0) break; 
+        buffer[i++] = c;
+        if (c == '\0') break; 
+    }
+    buffer[i] = '\0';
+    return (i > 0);
+}
