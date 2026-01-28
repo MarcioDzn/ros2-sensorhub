b3ad1e6b568b0214d9ca708ca82f20a73bc52dc1
MarcioDzn <marcio.roberto0739@gmail.com>
2026-01-28 11:26:47 -0300

    fix: pressureb byte reading stop

--- src/nodes/pressure_node/src/driver/insole/insole_driver.cpp
+++ src/nodes/pressure_node/src/driver/insole/insole_driver.cpp
@@ -58,7 +58,7 @@ bool InsoleDriver::read_c_string(char* buffer, size_t max_size)
         ssize_t n = transport_->readData(&c, 1);
         if (n <= 0) break; 
         buffer[i++] = c;
-        if (c == '\0') break; 
+        if (c == '\n') break; 
     }
     buffer[i] = '\0';
     return (i > 0);
