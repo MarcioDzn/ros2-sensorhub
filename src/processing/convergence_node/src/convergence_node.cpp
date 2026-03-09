62bf151a5e62ce111efa2e97c951e6dbf7547c29
i1 <i1@gmail.com>
2026-03-09 09:25:31 -0300

    refactor: add x axist to s

--- src/processing/convergence_node/src/convergence_node.cpp
+++ src/processing/convergence_node/src/convergence_node.cpp
@@ -15,20 +15,20 @@ ConvergenceNode::ConvergenceNode() : Node("convergence_node")
 void ConvergenceNode::time_callback(const interfaces::msg::SyncedSensorData::SharedPtr msg)
 {
     // Calcula tempo relativo
-    uint64_t current_time_ms = rclcpp::Time(msg->header.stamp).nanoseconds() / 1000000ULL;
-    if (start_time_ms_ == 0) start_time_ms_ = current_time_ms;
-    double relative_time_ms = static_cast<double>(current_time_ms - start_time_ms_);
+    double current_time_s = rclcpp::Time(msg->header.stamp).seconds();
+    if (start_time_ms_ == 0) start_time_ms_ = current_time_s;
+    double relative_time_s = static_cast<double>(current_time_s - start_time_ms_);
 
     // Janela 1: Juntas (usando o índice 1)
-    plotter_.add_data(1, "joint_1", relative_time_ms, static_cast<double>(msg->actuator_data.positions[0]));
-    plotter_.add_data(1, "joint_2", relative_time_ms, static_cast<double>(msg->actuator_data.positions[1]));
-    plotter_.add_data(1, "joint_3", relative_time_ms, static_cast<double>(msg->actuator_data.positions[2]));
+    plotter_.add_data(1, "joint_1", relative_time_s, static_cast<double>(msg->actuator_data.positions[0]));
+    plotter_.add_data(1, "joint_2", relative_time_s, static_cast<double>(msg->actuator_data.positions[1]));
+    plotter_.add_data(1, "joint_3", relative_time_s, static_cast<double>(msg->actuator_data.positions[2]));
     
     // Janela 2: IMU (usando o índice 2)
-    plotter_.add_data(2, "q_x", relative_time_ms, static_cast<double>(msg->imu_data.imus[0].q_x));
-    plotter_.add_data(2, "q_y", relative_time_ms, static_cast<double>(msg->imu_data.imus[0].q_y));
-    plotter_.add_data(2, "q_z", relative_time_ms, static_cast<double>(msg->imu_data.imus[0].q_z));
-    plotter_.add_data(2, "q_w", relative_time_ms, static_cast<double>(msg->imu_data.imus[0].q_w));
+    plotter_.add_data(2, "q_x", relative_time_s, static_cast<double>(msg->imu_data.imus[0].q_x));
+    plotter_.add_data(2, "q_y", relative_time_s, static_cast<double>(msg->imu_data.imus[0].q_y));
+    plotter_.add_data(2, "q_z", relative_time_s, static_cast<double>(msg->imu_data.imus[0].q_z));
+    plotter_.add_data(2, "q_w", relative_time_s, static_cast<double>(msg->imu_data.imus[0].q_w));
 
     // Rate limiting: plota a cada 20 mensagens para evitar crash no X11
     plotter_.plot();
