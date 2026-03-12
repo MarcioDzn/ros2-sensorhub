dd092574c69ea49310b80b0299c30194720a722a
MarcioDzn <marcio.roberto0739@gmail.com>
2026-03-12 08:56:37 -0300

    refactor: remove unit pressure data from msg assembly

--- src/nodes/pressure_node/src/pressure_node.cpp
+++ src/nodes/pressure_node/src/pressure_node.cpp
@@ -70,6 +70,7 @@ PressureState PressureNode::read_pressure_data(Time& time_data)
         
         auto duration = measure_micros([&]() {
             result = pressure_drivers_[idx]->get_data(data);
+            
         });
 
         time_data.names.push_back(names[idx]);
@@ -81,14 +82,10 @@ PressureState PressureNode::read_pressure_data(Time& time_data)
         // cria a mensagem
         PressureData pressure_data;
         pressure_data.pressures.reserve(data.size());
-
-        size_t sensor_id = 0;
+        
         for (auto value : data)
         {
-            PressureUnitSensor unit_sensor_data;
-            unit_sensor_data.id = sensor_id++;
-            unit_sensor_data.pressure = static_cast<int16_t>(value);
-            pressure_data.pressures.push_back(unit_sensor_data);
+            pressure_data.pressures.push_back(static_cast<int16_t>(value));
         }
 
         state_data.pressures.push_back(pressure_data);
