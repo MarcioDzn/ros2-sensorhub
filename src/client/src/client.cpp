e8fb3071f2fffaa326be0458d0692280caab278d
MarcioDzn <marcio.roberto0739@gmail.com>
2026-05-11 10:04:28 -0300

    feat: add angle path loop

--- src/client/src/client.cpp
+++ src/client/src/client.cpp
@@ -24,18 +24,22 @@ void ClientNode::run(int argc, char **argv)
 // Função principal de execução
 void ClientNode::execute_path()
 {
-    // Configura delays entre tópicos (em milissegundos)
-    std::vector<int> delays_ms = {0, 1, 1}; 
-
-    RCLCPP_INFO(this->get_logger(), "Enviando posicao.");
-    interfaces::msg::MG8008ECommand actuator_msg;
-    actuator_msg.names.push_back("joint_1");
-    actuator_msg.angles.push_back(500);
-    actuator_msg.speeds.push_back(360);
-    actuator_publisher_->publish(actuator_msg);
-    RCLCPP_INFO(this->get_logger(), "Posicao enviada.");
+    std::vector<int32_t> angle_path = {-100, 900, -100, 900, -100};
+    for (size_t i = 0; i < angle_path.size(); i++)
+    {
+        RCLCPP_INFO(this->get_logger(), "Enviando posicao.");
+        interfaces::msg::MG8008ECommand actuator_msg;
+        actuator_msg.names.push_back("joint_1");
+        actuator_msg.angles.push_back(angle_path[i]);
+        actuator_msg.speeds.push_back(360);
+        actuator_publisher_->publish(actuator_msg);
+        RCLCPP_INFO(this->get_logger(), "Posicao enviada.");
+        
+        rclcpp::sleep_for(std::chrono::seconds(2));
+    }
     
-    rclcpp::sleep_for(std::chrono::seconds(2));
+    RCLCPP_INFO(this->get_logger(), "Trajetoria concluida");
+
 }
 
 ClientNode::~ClientNode() {}
